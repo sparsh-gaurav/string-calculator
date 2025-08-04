18c5dd9e122c5f12fa4594afd13183d0db7e89d5
sparsh-gaurav <sparshgaurav123@gmail.com>
2025-08-04 22:22:21 +0530

    Add test case for custom delimiter handling in add function

--- Tests/test_string_calculator.py
+++ Tests/test_string_calculator.py
@@ -16,6 +16,9 @@ class TestStringCalculator(unittest.TestCase):
 
     def test_newline_and_comma_are_treated_as_delimiters(self):
          self.assertEqual(add("1\n2,3"), 6)
+    
+    def test_custom_delimiter_in_the_add_method(self):
+         self.assertEqual(add("//;\n1;2"), 3)
 
 if __name__ == "__main__":
     unittest.main()
