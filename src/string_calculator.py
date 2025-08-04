5e6261181fc452340a263ee415be42ea0dfb2d12
sparsh-gaurav <sparshgaurav123@gmail.com>
2025-08-04 22:25:47 +0530

    Refactor add function to support custom delimiters and improve newline handling

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,6 +1,11 @@
 def add(numbers: str | None) -> int:
     if not numbers:
         return 0
-    numbers = numbers.replace("\n", ",")
-    number_list = numbers.split(",")
+    delimiter = ","
+    if numbers.startswith("//"):
+        header, numbers = numbers.split("\n", 1)
+        delimiter = header[2:]
+
+    numbers = numbers.replace('\n', delimiter)
+    number_list = numbers.split(delimiter)
     return sum(int(n) for n in number_list)
